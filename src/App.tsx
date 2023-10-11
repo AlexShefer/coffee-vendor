7a86d7b25944305fcd05f2021529714a88930a7e
Aleksandr <Avshef451987@gmail.com>
2023-10-12 03:33:50 +0500

    Initial commit: Set up project for coffee vending machine dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Tabs } from "./components/Tabs";
+import { Dashboard } from "./components/dashboard/Dashboard";
+import { Table } from "./components/table/Table";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Tabs />
+            <Table />
+            <Dashboard />
+        </div>
+    );
 }
 
 export default App;
