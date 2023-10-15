1976238df597f6933850797e38feb331a5d6242a
Aleksandr <Avshef451987@gmail.com>
2023-10-15 23:44:44 +0500

    feat: Added modal component with input for setting a new date period for data filtering

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,125 @@
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { changeDateFormat } from "../../utils/helperFunctions";
+import "./modal.css";
+
+type ModalProps = {
+    onClose: () => void;
+    updateFilter: React.Dispatch<
+        React.SetStateAction<{
+            from: string;
+            to: string;
+        }>
+    >;
+    setInputDateFromTo: React.Dispatch<
+        React.SetStateAction<{
+            from: string;
+            to: string;
+        }>
+    >;
+    value: {
+        from: string;
+        to: string;
+    };
+};
+
+export const Modal: React.FC<ModalProps> = ({
+    value,
+    setInputDateFromTo,
+    onClose,
+    updateFilter,
+}) => {
+    const [modalHeader, setModalHeader] = useState(
+        "Выберите период времени для анализа данных:"
+    );
+    useEffect(() => {
+        document.body.classList.add("overflow-hidden");
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, []);
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        setInputDateFromTo((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    const handleUpdateFilter = () => {
+        if (value.from !== "" && value.to !== "") {
+            const fromDate = new Date(value.from);
+            const toDate = new Date(value.to);
+            if (fromDate > toDate) {
+                setModalHeader(
+                    "Ошибка ввода данных. Дата начала должна быть раньше даты окончания."
+                );
+                return;
+            }
+            const updatedDate = {
+                from: changeDateFormat(value.from),
+                to: changeDateFormat(value.to),
+            };
+
+            updateFilter(updatedDate);
+            onClose();
+        } else {
+            setModalHeader(
+                "Ошибка ввода данных. Оба поля должны быть заполнены."
+            );
+        }
+    };
+
+    return ReactDOM.createPortal(
+        <div className="modal">
+            <div onClick={onClose} className="modal__overlay"></div>
+            <div className="modal__content">
+                <div className="modal__container">
+                    <div className="modal__inner">
+                        <div className="modal__body">
+                            <h3 className="modal__body__header">
+                                {modalHeader}
+                            </h3>
+                            <div className="modal__body__input-group">
+                                <label htmlFor="from">Начиная с:</label>
+                                <input
+                                    type="date"
+                                    name="from"
+                                    value={value.from}
+                                    onChange={(e) => handleInputChange(e)}
+                                    id="from"
+                                    min="2023-01-01"
+                                    max="2023-12-31"
+                                />
+                                <label htmlFor="to">До:</label>
+                                <input
+                                    type="date"
+                                    name="to"
+                                    value={value.to}
+                                    onChange={(e) => handleInputChange(e)}
+                                    id="to"
+                                    min="2023-01-01"
+                                    max="2023-12-31"
+                                />
+                            </div>
+                            <button
+                                className="modal__body__close"
+                                onClick={onClose}
+                            >
+                                Close
+                            </button>
+                            <button
+                                className="modal__body__close"
+                                onClick={handleUpdateFilter}
+                            >
+                                Update Filter
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>,
+        document.querySelector(".modal-container") as HTMLElement
+    );
+};
