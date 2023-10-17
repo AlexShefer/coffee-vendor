ef712c1820efc45f86f496bf59e005f04c608e0f
Aleksandr <Avshef451987@gmail.com>
2023-10-17 18:31:37 +0500

    Added dispensing chart component and dispensing card component

--- src/utils/getCurrentWeekData.tsx
+++ src/utils/getCurrentWeekData.tsx
@@ -60,10 +60,6 @@ const filterDataByDate = (
         return itemDate >= startDate && itemDate <= endDate;
     });
 };
-const convertToDate = (dateString: string): Date => {
-    const [year, month, day] = dateString.split("-").map(Number);
-    return new Date(year, month - 1, day); // Month is 0-indexed
-};
 
 const getWeekData = (
     data: DataType[],
